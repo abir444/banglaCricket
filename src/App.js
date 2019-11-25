8e6f320cc5c9e351853aa2af407005b9f2d8331d
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2019-11-25 09:31:20 -0600

    todo added

--- src/App.js
+++ src/App.js
@@ -1,60 +1,70 @@
-import React, { Component } from 'react';
-import DateTimePicker from 'react-datetime-picker';
-import './App.css';
-import Login from './Login';
-import Register from './Register';
+import React, { Component } from "react";
+import DateTimePicker from "react-datetime-picker";
+import "./App.css";
+import Login from "./Login";
+import Register from "./Register";
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      list : [],
+      list: [],
+      toggler: "Login"
       // uname : '',
       // fname : '',
       // Lname : '',
       // email : '',
       // psw   : '',
       // date: new Date(),
-      //isLogin : false,  
+      //isLogin : false,
     };
-    this.handleChange=this.handleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     // this.submit=this.submit.bind(this);
   }
-  handleChange(event){
+
+  //TODO: This is supposed to be a onSubmit function and not onChange
+
+  // So call this onSubmit function on your Submit button in Register
+  // Also name it onRegisterSubmit (it's more descriptive)
+  // This will make your code more readable
+
+  //TODO: Also I would like to see just Login or just Register at a time
+  // so utilize the state "toggler", when you click on Register, you can see just register
+
+  handleChange(event) {
     let name = event.target.name;
     let value = event.target.value;
     let list = [];
-    let data =[];
-   // let d =  new Date();
-    data[name]=value;
+    let data = [];
+    // let d =  new Date();
+    data[name] = value;
     list.push({
-      userName : value.uname,
-      fullName : value.fname,
-      lastName : value.lname,
-      email    : value.email,
-      dob      : value.date,
-      password : value.psw,
-
+      userName: value.uname,
+      fullName: value.fname,
+      lastName: value.lname,
+      email: value.email,
+      dob: value.date,
+      password: value.psw
     });
-    this.setState({
-      list: event,
-      
-    },console.log(event),
-    )
-    
+    this.setState(
+      {
+        list: event
+      },
+      console.log(event)
+    );
   }
 
   render() {
     return (
-    
-        <div className = "App">
+      <div className="App">
         <div>
-        <Login/>
+          <Login />
         </div>
         <div>
-        <Register/>
+          <Register />
         </div>
-        </div>    
+        {/* This is just to view how your list looks like */}
+        {JSON.stringify(this.state.list)}
+      </div>
     );
   }
 }
--- src/Register.js
+++ src/Register.js
@@ -1,42 +1,89 @@
-import React from 'react';
-import './App.css';
-import DateTimePicker from 'react-datetime-picker';
-const Register = (props) => {
+import React from "react";
+import "./App.css";
+import DateTimePicker from "react-datetime-picker";
 
+//TODO: Change this to a class component
+// onChange should be here
+// Create state for every values.
+// this.state = {
+//   username,
+//   firstName
+//   lastName
+//   email
+//   password
+//   date
+// }
 
+// Look on Stackoverflow how to setState onChange
+// Here is the answer though, but try to look first online
+// Example onChange={(event)=> this.setState(username:event.target.value)}
 
-    return (
-      
-        <div className = "AppR">
-          <form onSubmit ={props.submit}>
-           <h3>Bangla Cricket Team</h3>
-           <div>
-            <div>Register</div>
-            <input type="text" placeholder="Username" value={props.uname} onChange={props.handleChange} name="uname" required/>
-            <br/>
-            <input type="text" placeholder="First Name" value={props.fname} onChange={props.handleChange} name="fname" required/>
-            <br/>
-            <input type="text" placeholder="Last Name" value={props.lname} onChange={props.handleChange}  name="lname" required/>
-            <br/>
-            <input type="text" placeholder="Email" value={props.email} onChange={props.handleChange}  name="email" required/>
-            <br/>
-            <input type="password" placeholder="Enter Password" value={props.psw} onChange={props.handleChange}  name="psw" required/>
-            <div>
-            <DateTimePicker name = "date" value={props.date}
-             handleChange={props.handleChange}
-            
+// In your onSubmit call that props.onRegisterSubmit, and you will need to pass
+// appropriate data
+
+const Register = props => {
+  return (
+    <div className="AppR">
+      <form onSubmit={props.submit}>
+        <h3>Bangla Cricket Team</h3>
+        <div>
+          <div>Register</div>
+          <input
+            type="text"
+            placeholder="Username"
+            value={props.uname}
+            onChange={props.handleChange}
+            name="uname"
+            required
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="First Name"
+            value={props.fname}
+            onChange={props.handleChange}
+            name="fname"
+            required
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={props.lname}
+            onChange={props.handleChange}
+            name="lname"
+            required
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Email"
+            value={props.email}
+            onChange={props.handleChange}
+            name="email"
+            required
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={props.psw}
+            onChange={props.handleChange}
+            name="psw"
+            required
+          />
+          <div>
+            <DateTimePicker
+              name="date"
+              value={props.date}
+              handleChange={props.handleChange}
             />
-            </div>
-            </div>
-            <input type="submit" value="Submit" />
-          </form>
-        
+          </div>
         </div>
-    );
-  }
-
-
-
-
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
 
 export default Register;
